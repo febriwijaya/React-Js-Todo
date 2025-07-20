60e8e93b7431701d695410fd090d63ecb35baa63
febri wijaya <59653659+febriwijaya@users.noreply.github.com>
2025-07-20 23:22:56 +0700

    add register page

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -4,6 +4,7 @@ import HeaderComponent from "./components/HeaderComponent";
 import FooterComponent from "./components/FooterComponent";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import TodoComponent from "./components/TodoComponent";
+import RegisterComponent from "./components/RegisterComponent";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/todos" element={<ListTodoComponent />}></Route>
           <Route path="/add-todo" element={<TodoComponent />}></Route>
           <Route path="/update-todo/:id" element={<TodoComponent />}></Route>
+          <Route path="/register" element={<RegisterComponent />}></Route>
         </Routes>
         <FooterComponent />
       </BrowserRouter>
